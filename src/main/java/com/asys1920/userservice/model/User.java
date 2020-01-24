a04f12494295d7e9f9190fe9b56b8cb59cc21176
AndreasKlar <andreas.klar@th-bingen.de>
2020-01-24 09:55:13 +0100

    Port changed to 8081
    Changed the default port of the service to 8081

--- src/main/java/com/asys1920/userservice/model/User.java
+++ src/main/java/com/asys1920/userservice/model/User.java
@@ -1,22 +1,11 @@
 package com.asys1920.userservice.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import net.minidev.json.annotate.JsonIgnore;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 
 @Data
 @Entity
@@ -32,4 +21,8 @@ public class User {
     boolean isActive;
     boolean isBanned;
 
+    String street;
+    String zipCode;
+    String city;
+    String country;
 }

771e1c06b18a8c44e89f6af2fe7cf8ef2664b5eb
Thomas <thomas.hormes@hotmail.de>
2020-02-27 14:27:00 +0100

    Added GracefulShutdown

--- src/main/java/com/asys1920/service/UserServiceApplication.java
+++ src/main/java/com/asys1920/service/UserServiceApplication.java
@@ -1,9 +1,13 @@
 package com.asys1920.service;
 
+import com.asys1920.util.GracefulShutdown;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.validation.Validator;
@@ -26,5 +30,17 @@ public class UserServiceApplication implements RepositoryRestConfigurer {
     public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener v) {
         v.addValidator("beforeCreate", validator);
         v.addValidator("beforeSave", validator);
+    }
+
+    @Bean
+    public GracefulShutdown gracefulShutdown() {
+        return new GracefulShutdown();
+    }
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory(final GracefulShutdown gracefulShutdown) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(gracefulShutdown);
+        return factory;
     }
 }
